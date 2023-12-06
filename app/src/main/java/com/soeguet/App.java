15bd5b0685476456bd753e3efba43b8d9962ec61
soeguet <osman_s@live.de>
2023-12-06 22:09:55 +0100

    day_01_02 check

--- app/src/main/java/com/soeguet/App.java
+++ app/src/main/java/com/soeguet/App.java
@@ -1,9 +1,11 @@
 package com.soeguet;
 
+import com.soeguet.day01.Day_01_02;
+
 public class App {
 
     public static void main(String[] args) {
         
-        new Day_01_01();
+        new Day_01_02();
     }
 }
