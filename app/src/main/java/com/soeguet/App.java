027bb47bf7f969e0ff67b4d6ba8723f3ca886855
soeguet <osman_s@live.de>
2023-12-10 22:18:38 +0100

    started with day 03 01

--- app/src/main/java/com/soeguet/App.java
+++ app/src/main/java/com/soeguet/App.java
@@ -1,10 +1,10 @@
 package com.soeguet;
 
-import com.soeguet.day02.Day_02_02;
+import com.soeguet.day03.Day_03_01;
 
 public class App {
 
     public static void main(String[] args) {
-        new Day_02_02();
+        new Day_03_01();
     }
 }
--- app/src/main/java/com/soeguet/day03/Day_03_01.java
+++ app/src/main/java/com/soeguet/day03/Day_03_01.java
@@ -1,9 +1,82 @@
 package com.soeguet.day03;
 
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class Day_03_01 {
+
     public Day_03_01() {
-        System.out.println("henlo");
-        int a =1;
-        System.out.println(a);
+
+        String input = getInput();
+        Stream<String> lines = input.lines();
+        ArrayList<Point> pointColumnList = new ArrayList<>();
+        ArrayList<ArrayList<Point>> rowList = new ArrayList<>();
+
+        lines.forEach(line -> {
+
+            char[] charArray = line.toCharArray();
+
+            String concatValue = "";
+
+            for (int i = 0; i < charArray.length; i++) {
+
+                char abc = charArray[i];
+
+                if (Character.isDigit(abc)){
+                    
+
+                    concatValue = concatValue + abc;
+                }
+                else if (abc == '.') {
+                    
+                    concatValue = "";
+                }
+                else if (!Character.isLetterOrDigit(abc)) {
+                    
+                }
+            }
+        });
+
+        System.out.println(input);
     }
+
+    public class Point {
+
+        private int row;
+        private int column;
+        private Type type;
+        private int value;
+        private boolean evaluate = false;
+
+        public int lookForValue() {
+
+            if (evaluate) {
+
+                return value;
+            }
+
+            return 0;
+        }
+    }
+
+    enum Type {
+        NONE, DIGIT, CHAR
+    }
+
+    private String getInput() {
+        return """
+                                    467..114..
+                                    ...*......
+                                    ..35..633.
+                                    ......#...
+                                    617*......
+                                    .....+.58.
+                                    ..592.....
+                                    ......755.
+                                    ...$.*....
+                                    .664.598..
+                """;
+
+    }
+
 }
