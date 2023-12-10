91e307a46afcc01cfaaa858f94f14ecd674c2a3c
soeguet <osman_s@live.de>
2023-12-10 20:16:08 +0100

    day 2 solved

--- app/src/main/java/com/soeguet/App.java
+++ app/src/main/java/com/soeguet/App.java
@@ -1,10 +1,10 @@
 package com.soeguet;
 
-import com.soeguet.day02.Day_02_01;
+import com.soeguet.day02.Day_02_02;
 
 public class App {
 
     public static void main(String[] args) {
-        new Day_02_01();
+        new Day_02_02();
     }
 }
